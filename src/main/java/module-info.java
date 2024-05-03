563d0fd9087ebf5abb42220eccc4939678a1eb0f
afrimymeri <afrimymeri0@gmail.com>
2024-05-03 16:17:10 +0200

    Initial commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,13 @@
 module com.example.knk_g6 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
     requires org.kordamp.bootstrapfx.core;
 
     opens com.example.knk_g6 to javafx.fxml;
+
     exports com.example.knk_g6;
+
+
 }
