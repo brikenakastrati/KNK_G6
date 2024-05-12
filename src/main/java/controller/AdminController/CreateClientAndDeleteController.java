4ebde2befac8816b92792456c13f11eded126061
Afrim Ymeri <afrimymeri0@gmail.com>
2024-05-12 17:24:52 +0200

    Creating client from adminclients page

--- src/main/java/controller/AdminController/CreateClientAndDeleteController.java
+++ src/main/java/controller/AdminController/CreateClientAndDeleteController.java
@@ -1,11 +1,14 @@
 package controller.AdminController;
 
+import Repository.UserRepository;
 import app.Navigator;
+import controller.SignUpController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import model.User;
+import model.dto.UserDto;
+import service.UserService;
 
 public class CreateClientAndDeleteController {
     @FXML
@@ -13,7 +16,61 @@ public class CreateClientAndDeleteController {
     @FXML
     private PasswordField txtpassword, txtconfirmpassword;
     @FXML
+    private Button deleteBtn, createclientbutton;
+    @FXML
     private TableView usertable;
+
+    @FXML
+    private void handleCreateClient(ActionEvent ae){
+
+
+        if (txtusername.getText().isEmpty() || txtemail.getText().isEmpty() || txtpassword.getText().isEmpty() ||
+                txtconfirmpassword.getText().isEmpty()) {
+            showAlert(Alert.AlertType.ERROR, "Creation error", "Please fill in all the fields");
+            return;
+        }
+
+        if (!txtemail.getText().matches("^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$")) {
+            showAlert(Alert.AlertType.ERROR, "Creation error", "Please enter a valid email address");
+            return;
+        }
+        if (!txtpassword.getText().equals(txtconfirmpassword.getText())) {
+            showAlert(Alert.AlertType.ERROR, "Creation error", "Passwords do not match");
+            return;
+        }
+        if (txtpassword.getText().length() < 8) {
+            showAlert(Alert.AlertType.ERROR, "Creation error", "Password must be at least 8 characters long");
+            return;
+        }
+        User user = UserRepository.getByUsername(this.txtusername.getText());
+        if (user != null) {
+            showAlert(Alert.AlertType.ERROR, "Creation error", "Username is already taken try another one");
+        }
+        User user2 = UserRepository.getByEmail(this.txtemail.getText());
+        if (user2 != null) {
+            showAlert(Alert.AlertType.ERROR, "Creation error", "Email address is already taken try another one");
+        }
+
+
+        UserDto userSignUpData = new UserDto(
+                this.txtusername.getText(),
+                this.txtemail.getText(),
+                this.txtpassword.getText(),
+                this.txtconfirmpassword.getText()
+        );
+
+        boolean response = UserService.createclientbutton(userSignUpData);
+
+        if(response){
+            showAlert(Alert.AlertType.INFORMATION, "New client created", "New client created successfully");
+            Navigator.navigate(ae, Navigator.AdminClients_page);
+        }
+        else {
+            showAlert(Alert.AlertType.ERROR, "Creating client error", "Error while creating new client");
+        }
+
+    }
+
 
     @FXML
     private void handleClientsClick(ActionEvent ae) {
@@ -30,5 +87,13 @@ public class CreateClientAndDeleteController {
     @FXML
     private void handleDashboardClick(ActionEvent ae){
         Navigator.navigate(ae, Navigator.AdminDashboard_Page);
+    }
+
+    private void showAlert(Alert.AlertType alertType, String title, String content) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(title);
+        alert.setContentText(content);
+        alert.showAndWait();
     }
 }
