bb5134a9bc83ab732f012c922fa962faa7ea1f85
brikenakastrati <116557835+brikenakastrati@users.noreply.github.com>
2024-05-18 21:22:25 +0200

    changepwd fxml file added

---
+++ src/main/java/controller/ClientController/ChangePasswordController.java
@@ -0,0 +1,8 @@
+package controller.ClientController;
+
+import javafx.event.ActionEvent;
+
+public class ChangePasswordController {
+    public void handleChangePasswordClick(ActionEvent actionEvent) {
+    }
+}
