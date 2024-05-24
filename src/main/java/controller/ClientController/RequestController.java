233b70d3c360951ba5b001ffd743968034db4ee3
BrilantPonxhaj <brilant.ponxhaj@student.uni-pr.edu>
2024-05-24 09:52:07 +0200

    added the request for car page (might delete)

--- src/main/java/controller/ClientController/RequestController.java
+++ src/main/java/controller/ClientController/RequestController.java
@@ -3,32 +3,53 @@ package controller.ClientController;
 import app.Navigator;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import model.RestockRequest;
 import model.User;
+import service.RestockRequestService;
 import service.UserService;
 import service.UserSession;
 
 public class RequestController {
 
     UserService userService = new UserService();
+    RestockRequestService restockRequestService = new RestockRequestService();
+
+    @FXML
+    private TextField txtCarBrand;
+    @FXML
+    private TextField txtCarModel;
+    @FXML
+    private TextField txtCarName;
+    @FXML
+    private TextField txtCarType;
+    @FXML
+    private TextArea txtCarDescription;
 
     @FXML
     public void handleCarsClick(ActionEvent ae) {
         Navigator.navigate(ae, Navigator.CARS2_PAGE);
     }
+
     @FXML
     public void handleRequestCarClick(ActionEvent ae) {
         Navigator.navigate(ae, Navigator.REQUEST_CAR_PAGE);
     }
+
     @FXML
     public void handleCustomizeClick(ActionEvent ae) {
         Navigator.navigate(ae, Navigator.CUSTOMIZE_PAGE);
     }
+
     @FXML
     public void handleLogoutClick(ActionEvent ae) {
         UserSession.clearUserSession();
         Navigator.navigate(ae, Navigator.LOGIN_PAGE);
     }
+
     @FXML
     public void handleProfileClick(MouseEvent me) {
         try {
@@ -40,7 +61,46 @@ public class RequestController {
         }
     }
 
+    @FXML
     public void handleHelpClick(ActionEvent actionEvent) {
-        Navigator.navigate(actionEvent,Navigator.HELP_PAGE);
+        Navigator.navigate(actionEvent, Navigator.HELP_PAGE);
+    }
+
+    @FXML
+    public void handleSubmitRequest(ActionEvent actionEvent) {
+        String carBrand = txtCarBrand.getText();
+        String carModel = txtCarModel.getText();
+        String carName = txtCarName.getText();
+        String carType = txtCarType.getText();
+        String carDescription = txtCarDescription.getText();
+        String currentUser = UserService.getUsername();
+
+        if (carBrand.isEmpty() || carModel.isEmpty() || carName.isEmpty() || carType.isEmpty() || carDescription.isEmpty()) {
+            showAlert(Alert.AlertType.ERROR, "Form Error!", "Please fill in all fields");
+            return;
+        }
+
+        String combinedCarInfo = carBrand + " " + carModel + " +  (" + carType + "): " + carDescription;
+
+        restockRequestService.requestRestock(currentUser, combinedCarInfo);
+
+        showAlert(Alert.AlertType.INFORMATION, "Request Submitted", "Your car suggestion has been submitted!");
+
+        clearFields();}
+
+    private void clearFields() {
+        txtCarBrand.clear();
+        txtCarModel.clear();
+        txtCarName.clear();
+        txtCarType.clear();
+        txtCarDescription.clear();
+    }
+
+    private void showAlert(Alert.AlertType alertType, String title, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
     }
 }
